fbbdab3bbf0d7ee95bf4c2ffe64bad45fd4f53a4
Coach <nuttapong_coach@hotmail.com>
2024-05-14 15:31:40 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { jsPDF } from 'jspdf';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,36 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project-name';
+  dataService = inject(DataService);
+  generatePDF(){
+    const doc = new jsPDF();
+    doc.setFont('Helvetica');
+    doc.setFontSize(18);
+    doc.text(
+      `Monthly Sales Report for ${this.dataService.clientData.name}`,
+      60,
+      20
+    );
+    doc.setFontSize(12);
+    doc.text(`Reporting Period ${this.dataService.clientData.name}`, 60, 30);
+
+    // Add transactions
+    let yPos = 50;
+    this.dataService.clientData.transactions.forEach((transaction) => {
+      doc.text(
+        `${transaction.date} - ${
+          transaction.description
+        } $${transaction.amount.toFixed(2)}`,
+        10,
+        yPos
+      );
+      yPos += 10;
+    });
+
+    // Add total sales
+    doc.setFontSize(14);
+    doc.text(`Total Sales: $${this.dataService.totalSales.toFixed(2)}`, 10, yPos+20);
+
+    doc.save('monthly-report.pdf');
+  }
 }
